919c7028924889c5b6b226854c9237cd10be673d
HCOMG <hcomg.corp@gmail.com>
2017-04-11 11:57:24 +0700

    Add datatable back

--- src/views/layouts/master.blade.php
+++ src/views/layouts/master.blade.php
@@ -629,6 +629,9 @@
   <script src="/backend/web/assets/js/skins.min.js"></script>
   <script src="/backend/web/assets/js/bootstrap.min.js"></script>
   <script src="/backend/web/assets/js/slimscroll/jquery.slimscroll.min.js"></script>
+  <script src="//cdn.datatables.net/1.10.9/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.9/js/dataTables.bootstrap.min.js"></script>
+  <script src="https://cdn.datatables.net/responsive/2.0.0/js/dataTables.responsive.min.js"></script>
   <script src="/backend/web/assets/js/beyond.js"></script>
   <script src="/backend/web/assets/js/libs/bootbox.min.js"></script>
   <script src="/backend/web/assets/js/libs/confirm.js"></script>
