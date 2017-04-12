323a4079114cee9ecd14efc443817b520366ebb1
HCOMG <hcomg.corp@gmail.com>
2017-04-12 14:38:19 +0700

    Move chatbar, sidebar to other file

---
+++ src/views/partials/sidebar.blade.php
@@ -0,0 +1,223 @@
+<div class="page-sidebar" id="sidebar">
+  <!-- sidebar menu: : style can be found in sidebar.less -->
+  <ul class="nav sidebar-menu">
+    <li>
+      <a href="#" class="menu-dropdown">
+        <span class='menu-item'>
+        <span class='menu-icon'>
+        <i class="menu-icon fa fa-th"></i>
+        </span>
+        <span class='menu-label'>Phân hệ thống</span>
+        <i class="menu-expand"></i>
+        </span>
+      </a>
+      <ul class="submenu">
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Danh mục quyển chứng từ</span></a></li>
+        <li><a href="/backend/accounting/system/currency"><span
+              class="menu-text">Danh mục tiền tệ</span></a></li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Danh mục nhân viên</span></a></li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Khai báo ngày bắt đầu năm tài chính</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Danh mục tài khoản</span></a></li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Danh mục khách hàng</span></a></li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Danh mục vật tư</span></a></li>
+        <li><a href="/backend/accounting/default/#"><span class="menu-text">Danh mục kho hàng</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span class="menu-text">Danh mục vụ việc</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Danh mục loại chứng từ</span></a></li>
+        <li><a href="/backend/accounting/default/#"><span class="menu-text">Khóa số liệu</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Bảo trì số liệu</span></a></li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Khai báo người dùng và phân quyền truy cập</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Nhật ký người sử dụng</span></a></li>
+        <li><a href="/backend/accounting/default/#"><span class="menu-text">Danh mục thuế</span></a>
+        </li>
+      </ul>
+    </li>
+    <li>
+      <a href="#" class="menu-dropdown"><span class='menu-item'>
+            <span class='menu-icon'><i class="menu-icon fa fa-th"></i></span>
+            <span class='menu-label'>Phân tổng hợp</span>
+            <i class="menu-expand"></i>
+            </span></a>
+      <ul class="submenu">
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Cập nhật phiếu kế toán</span></a></li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Khai báo các bút toán kết chuyển cuối kỳ</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span class="menu-text">Bút toán kết chuyển cuối kỳ</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Vào số dư đầu kỳ của các tài khoản</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Vào số dư đầu kỳ các công nợ</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Vào số dư đầu kỳ các vụ việc</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Vào tồn kho đầu kỳ</span></a></li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Chuyển số dư tài khoản sang năm sau</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Chuyển số dư công nợ sang năm sau</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Chuyển số dư các vụ việc sang năm sau</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span class="menu-text">Chuyển tồn kho sang năm sau</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Bảng cân đối phát sinh các tài khoản</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Kết quả hoạt động kinh doanh</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Sổ chi tiết tài khoản</span></a></li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Sổ tổng hợp chữ T của 1 tài khoản</span></a>
+        </li>
+      </ul>
+    </li>
+    <li>
+      <a href="#" class="menu-dropdown"><span class='menu-item'>
+            <span class='menu-icon'><i class="menu-icon fa fa-th"></i></span>
+            <span class='menu-label'>Tiền mặt, tiền gửi ngân hàng</span>
+            <i class="menu-expand"></i>
+            </span></a>
+      <ul class="submenu">
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Phiếu thu tiền mặt</span></a></li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Phiếu chi tiền mặt</span></a></li>
+        <li><a href="/backend/accounting/default/#"><span class="menu-text">Qiấy báo có (thu) của ngân hàng</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span class="menu-text">Giấy báo nợ (chi) của ngân hàng</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span class="menu-text">Sổ quỹ</span></a></li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Sổ quỹ in theo ngày</span></a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#" class="menu-dropdown"><span class='menu-item'>
+            <span class='menu-icon'><i class="menu-icon fa fa-th"></i></span>
+            <span class='menu-label'>Mua bán hàng và công nợ phải thu, phải trả</span>
+            <i class="menu-expand"></i>
+            </span></a>
+      <ul class="submenu">
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Hóa đơn bán hàng kiêm phiếu xuất kho</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Hóa đơn dịch vụ</span></a></li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Phiếu nhập mua hàng</span></a></li>
+        <li><a href="/backend/accounting/default/#"><span class="menu-text">Phiếu nhập chi phí mua hàng</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span class="menu-text">Phiếu trả lại nhà cung cấp</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Bảng kê phiếu nhập hàng bán bị trả lại</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Bảng kê hóa đơn bán hàng và dịch vụ</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Báo cáo tổng hợp bán hàng và dịch vụ</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span class="menu-text">Báo cáo bán hàng 2 chỉ tiêu</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Báo cáo tổng hợp hàng bán bị trả lại</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Báo cáo doanh số theo khách hàng</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Bảng kê phiếu nhập</span></a></li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Bảng kê hóa đơn mua hàng và dịch vụ</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Bảng kê phiếu xuất trả lại nhà cung cấp</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span class="menu-text">Tổng hợp hàng hóa nhập mua</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Tổng hợp hàng xuất trả lại nhà cung cấp</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span class="menu-text">Báo cáo giá trị hàng nhập theo nhà cung cấp, vụ việc</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Báo cáo hàng nhập nhóm theo 2 chỉ tiêu</span></a>
+        </li>
+      </ul>
+    </li>
+    <li>
+      <a href="#" class="menu-dropdown">
+            <span class='menu-item'>
+            <span class='menu-icon'>
+            <i class="menu-icon fa fa-th"></i>
+            </span>
+            <span class='menu-label'>Kế toán hàng tồn kho</span>
+            <i class="menu-expand"></i>
+            </span>
+      </a>
+      <ul class="submenu">
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Phiếu nhập kho</span></a></li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Phiếu xuất kho</span></a></li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Phiếu điều chuyển kho</span></a></li>
+        <li><a href="/backend/accounting/default/#"><span class="menu-text">Thẻ kho/ Sổ chi tiết vật tư</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Tổng hợp nhập xuất tồn</span></a></li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Báo cáo tồn kho</span></a></li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Báo cáo tồn theo kho</span></a></li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Báo cáo tồn kho đầu kỳ</span></a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#" class="menu-dropdown"><span class='menu-item'>
+            <span class='menu-icon'><i class="menu-icon fa fa-th"></i></span>
+            <span class='menu-label'>Báo cáo vụ việc, giá thành</span>
+            <i class="menu-expand"></i>
+            </span></a>
+      <ul class="submenu">
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Sổ chi tiết vụ việc</span></a></li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Bảng kê chứng từ theo vụ việc</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span class="menu-text">Tổng hợp phát sinh theo vụ việc</span></a>
+        </li>
+        <li><a href="/backend/accounting/default/#"><span
+              class="menu-text">Bảng cân đối phát sinh các vụ việc</span></a>
+        </li>
+      </ul>
+    </li>
+  </ul>
+  <!-- /Sidebar Menu -->
+</div>
